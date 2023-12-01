add9e39a06d498ac0fdbcaaf409a508d97b9c4b9
Your Name <you@example.com>
2023-11-30 23:39:19 -0600

    11/30

--- Listado_de_personas/JS/app.js
+++ Listado_de_personas/JS/app.js
@@ -1,1 +1,14 @@
-console.log('Cargando app.js...')
+const personas = [
+  new Persona('Juan', 'Perez', 46),
+  new Persona('Laura', 'Quintero', 36),
+  new Persona('Carlos', 'Lara', 25),
+];
+
+function mostrarPersonas(){
+  console.log('Mostrando personas...');
+  let texto = '';
+  for(let persona of personas){
+    texto += `<li>${persona.nombre} ${persona.apellido} (${persona.edad})</li>`;
+  }
+  document.getElementById('personas').innerHTML = texto;
+}
--- Listado_de_personas/JS/personas.js
+++ Listado_de_personas/JS/personas.js
@@ -1,1 +1,16 @@
-console.log('Cargando personas.js...');
+class Persona{
+  constructor(nombre, apellido, edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+
+  get nombre(){return this._nombre;}
+  set nombre(nombre){this._nombre = nombre;}
+
+  get apellido(){return this._apellido;}
+  set apellido(apellido){this._apellido = apellido;}
+
+  get edad(){return this._edad;}
+  set edad(edad){this._edad = edad;}
+}
